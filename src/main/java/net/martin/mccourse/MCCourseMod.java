4eaf4b4018a163d6e8fb4879333e26a1fa9fd944
metalmod <schaulin.martin@gmail.com>
2022-03-07 17:59:50 +0100

    Added a comment for learning commit with Git

--- src/main/java/net/martin/mccourse/MCCourseMod.java
+++ src/main/java/net/martin/mccourse/MCCourseMod.java
@@ -16,7 +16,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
-
+//Added a comment here for learning commit with Git
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(MCCourseMod.MOD_ID)
 public class MCCourseMod
